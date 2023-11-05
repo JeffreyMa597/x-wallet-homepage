1c89192b72a598427d990a89d01ed80c2d15f0a2
Jun Ma <ma82180597@163.com>
2023-11-05 22:33:09 +0800

    feat: update ui

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,13 +1,35 @@
 import Image from 'next/image';
-import styles from './Content.module.scss';
 import Banner from '@/assets/images/banner.jpg';
+import XLogo from '@/assets/images/xlogo.png';
+import ScreenShot from '@/assets/images/screenShot.png';
+import RightArrow from '@/assets/images/rightArrow.png';
+import Btc from '@/assets/images/btc.png';
+import Link from '@/assets/images/link.png';
+import Eth from '@/assets/images/eth.png';
+
+import styles from './Content.module.scss';
 
 export default function Content() {
   const partnerList = [
     {
-      label: 'Btc',
-      icon: '',
-      // icon: 'https://raw.githubusercontent.com/0xLukin/x-wallet-ethhangzhou/main/src/pages/Popup/assets/svg/setting.png',
+      label: 'Btc1',
+      icon: Btc,
+    },
+    {
+      label: 'Link1',
+      icon: Link,
+    },
+    {
+      label: 'Eth',
+      icon: Eth,
+    },
+    {
+      label: 'Btc2',
+      icon: Btc,
+    },
+    {
+      label: 'Link2',
+      icon: Link,
     },
   ];
 
@@ -26,16 +48,38 @@ export default function Content() {
         </div>
       </div>
 
-      <div className={styles.createWrap}>imitate</div>
+      <div className={styles.createWrap}>
+        <div className={styles.createBox}>
+          <div className={styles.leftPart}>
+            <div className={styles.mainTitle}>Imitate</div>
+            <div className={styles.subTitle}>Pre-caculate twitter user AA account.</div>
+            <div className={styles.createInputWrap}>
+              <Image className={styles.xlogo} src={XLogo} alt="xlogo" width={175} height={50} />
+              <div className={styles.createInput}>
+                <input type="text" />
+                <div className={styles.rightArrowWrap}>
+                  <Image src={RightArrow} alt="rightArrow" width={24} height={26} />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.rightPart}>
+            <Image src={ScreenShot} alt="screenShot" width={320} height={340} />
+          </div>
+        </div>
+      </div>
 
       <div className={styles.partnerWrap}>
         <div className={styles.partnerTitle}>Partners</div>
-        {partnerList?.map((item) => (
-          <div key={item.icon} className={styles.partnerItem}>
-            <Image src={item.icon} alt="icon" layout="responsive" width={10} height={10} />
-          </div>
-        ))}
+        <div className={styles.iconsBox}>
+          {partnerList?.map((item) => (
+            <div key={item.label} className={styles.partnerItem}>
+              <Image src={item.icon} alt="icon" width={40} height={40} />
+            </div>
+          ))}
+        </div>
       </div>
+      <div className={styles.footer}></div>
     </div>
   );
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,19 @@
+import Image from 'next/image';
+import Logo from '@/assets/images/logo.png';
+import Twitter from '@/assets/images/twitter.png';
+import Discord from '@/assets/images/discord.png';
+
 import styles from './Header.module.scss';
 export default function Header() {
   return (
     <div className={styles.headerWrap}>
-      <div>x-wallet Header Logo</div>
-      <div>Media</div>
+      <div className={styles.headerBox}>
+        <Image src={Logo} alt="Logo" />
+        <div className={styles.mediaWrap}>
+          <Image src={Twitter} alt="Twitter" />
+          <Image src={Discord} alt="Discord" />
+        </div>
+      </div>
     </div>
   );
 }
